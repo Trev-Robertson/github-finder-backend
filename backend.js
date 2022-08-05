a7aefa9bed08ca35df61271489c793a467f4e5a1
Trevahr R <trevahrr@ncyber.com>
2022-08-04 20:00:48 -0400

    commented out unused routes and token

--- backend.js
+++ backend.js
@@ -13,9 +13,10 @@ appExpress.use(cors())
 appExpress.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 
-appExpress.get('/token', (req, res, next) => {
-   res.json({token: TOKEN})
-})
+// appExpress.get('/token', (req, res, next) => {
+//     console.log('TOKEN HIT');
+//    res.json({token: TOKEN})
+// })
 
 
 appExpress.get('/search/users', (req, res, next) => {
@@ -26,53 +27,53 @@ appExpress.get('/search/users', (req, res, next) => {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
     }).then(data => {
-        // console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
-        // console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
+        console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
+        console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
         res.json(data.data)
     }).catch(function (error) {
-        // console.log("ERROR", error);
+        console.log("ERROR", error);
         // res.json(error)
         next(error)
     })
 })
 
 
-appExpress.get('/users', (req, res, next) => {
-    const url2 = GITHUB_API + req.url
-    console.log('URL2', url2);
-    axios.request(url2, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-    }).then(data => {
-        // console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
-        // console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
-        res.json(data.data)
-    }).catch(function (error) {
-        next(error)
-        // console.log("ERROR", error.response.status);
-        // res.sendStatus(error.response.status)
-        // res.json(error.message)
-    })
+// appExpress.get('/users', (req, res, next) => {
+//     const url2 = GITHUB_API + req.url
+//     console.log('URL2', url2);
+//     axios.request(url2, {
+//             headers: {
+//                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+//             }
+//     }).then(data => {
+//         // console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
+//         // console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
+//         res.json(data.data)
+//     }).catch(function (error) {
+//         next(error)
+//         // console.log("ERROR", error.response.status);
+//         // res.sendStatus(error.response.status)
+//         // res.json(error.message)
+//     })
     
-})
+// })
 
-appExpress.get('/users/*', (req, res, next) => {
-    const url2 = GITHUB_API + req.url
-    // console.log('REQ', req.url);
-    console.log('URL2', url2);
-   const a =  axios.request(url2, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-    }).then(data => {
-        // console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
-        // console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
-        console.log('DATA', data.data)
-        res.json(data.data)
-    }).catch(function (error) {
-        next(error)
-        // console.log("ERROR", error);
-        // res.json(error)
-    })
-})
+// appExpress.get('/users/*', (req, res, next) => {
+//     const url2 = GITHUB_API + req.url
+//     // console.log('REQ', req.url);
+//     console.log('URL2', url2);
+//    const a =  axios.request(url2, {
+//             headers: {
+//                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+//             }
+//     }).then(data => {
+//         // console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
+//         // console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
+//         console.log('DATA', data.data)
+//         res.json(data.data)
+//     }).catch(function (error) {
+//         next(error)
+//         // console.log("ERROR", error);
+//         // res.json(error)
+//     })
+// })
