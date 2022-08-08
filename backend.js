4b734b790494c534022fd858fd78074c3cd33498
Trevahr R <trevahrr@ncyber.com>
2022-08-08 16:36:57 -0400

    added repos routing

--- backend.js
+++ backend.js
@@ -27,10 +27,11 @@ appExpress.get('/search/users', (req, res, next) => {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
     }).then(data => {
-        console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
-        console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
+        console.log('HEADER RATE LIMIT /search/users', data.headers['x-ratelimit-limit'])
+        console.log('HEADER RATE REMAINING /search/users', data.headers['x-ratelimit-remaining'])
         res.json(data.data)
     }).catch(function (error) {
+        console.log('ERRORSEARCH', error);
         res.status(error.response.status)
         res.json(error)
     })
@@ -45,8 +46,26 @@ appExpress.get('/users/:login', (req, res, next) => {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
     }).then(data => {
-        console.log('HEADER RATE LIMIT', data.headers['x-ratelimit-limit'])
-        console.log('HEADER RATE REMAINING', data.headers['x-ratelimit-remaining'])
+        console.log('HEADER RATE LIMIT /users/:login', data.headers['x-ratelimit-limit'])
+        console.log('HEADER RATE REMAINING /users/:login', data.headers['x-ratelimit-remaining'])
+        res.json(data.data)
+    }).catch(function (error) {
+        res.status(error.response.status)
+        res.json(error)
+    })
+})
+
+appExpress.get('/users/:login/repos', (req, res, next) => {
+    const url2 = GITHUB_API + req.url 
+    // + '?sort=created&direction=dsc&per_page=10'
+    // console.log('REQ', url2);
+    axios.request(url2, {
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+    }).then(data => {
+        console.log('HEADER RATE LIMIT /users/:login/repos', data.headers['x-ratelimit-limit'])
+        console.log('HEADER RATE REMAINING /users/:login/repos', data.headers['x-ratelimit-remaining'])
         res.json(data.data)
     }).catch(function (error) {
         res.status(error.response.status)
