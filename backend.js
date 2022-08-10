4c4f7ff40a76ec363503c460b09eb68f610a7e70
Trevahr R <trevahrr@ncyber.com>
2022-08-10 15:25:37 -0400

    removed a console log

--- backend.js
+++ backend.js
@@ -26,7 +26,6 @@ appExpress.get('/search/users', (req, res, next) => {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
     }).then(data => {
-        console.log('HEADER RATE LIMIT /search/users', data.headers['x-ratelimit-limit'])
         console.log('HEADER RATE REMAINING /search/users', data.headers['x-ratelimit-remaining'])
         res.status(data.status)
         res.json(data.data)
@@ -51,7 +50,6 @@ appExpress.get('/users/:login', (req, res, next) => {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
     }).then(data => {
-        console.log('HEADER RATE LIMIT /users/:login', data.headers['x-ratelimit-limit'])
         console.log('HEADER RATE REMAINING /users/:login', data.headers['x-ratelimit-remaining'])
         res.status(data.status)
         res.json(data.data)
@@ -74,7 +72,6 @@ appExpress.get('/users/:login/repos', (req, res, next) => {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
     }).then(data => {
-        console.log('HEADER RATE LIMIT /users/:login/repos', data.headers['x-ratelimit-limit'])
         console.log('HEADER RATE REMAINING /users/:login/repos', data.headers['x-ratelimit-remaining'])
         res.status(data.status)
         res.json(data.data)
